425642a2cbbdf32a38b82ded8377860b97ce04e2
sujinpark50-gif <sujinpark50@gmail.com>
2026-07-08 16:20:59 +0900

    반복문 예제 추가

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-message = "안녕하세요"
-print(message)
+while True:
+    name = input("이름을 입력하세요(종료는 q): ")
 
-name = input("이름을 입력하세요: ")
-age = int(input("나이를 입력하세요: "))
+    if name == "q":
+        break
 
-print("안녕하세요 " + name + "님!")
-
-if age >= 20:
-    print("성인입니다.")
-else:
-    print("미성년자입니다.")
+    print("안녕하세요", name)
