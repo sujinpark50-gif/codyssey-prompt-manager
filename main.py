0f17bf3fa854107b17641ede2d4354bfa3a0288c
sujinpark50-gif <sujinpark50@gmail.com>
2026-07-08 16:51:18 +0900

    딕셔너리 예제 추가

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-prompts = [
-    "블로그 글",
-    "광고 문구",
-    "이미지 생성"
-]
+prompt = {
+    "title": "블로그 글",
+    "category": "텍스트 생성",
+    "favorite": True
+}
 
-print(prompts)
-print(prompts[0])
-print(prompts[1])
-print(prompts[2])
+print(prompt["title"])
