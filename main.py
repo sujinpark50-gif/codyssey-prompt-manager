9ce2ebcf465dfb56e135209ddb37957dd784eba5
sujinpark50-gif <sujinpark50@gmail.com>
2026-07-08 16:08:27 +0900

    조건문 예제 추가

--- main.py
+++ main.py
@@ -5,5 +5,8 @@ name = input("이름을 입력하세요: ")
 age = int(input("나이를 입력하세요: "))
 
 print("안녕하세요 " + name + "님!")
-print("내년 나이는")
-print(age + 1)
+
+if age >= 20:
+    print("성인입니다.")
+else:
+    print("미성년자입니다.")
